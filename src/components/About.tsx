0dcd9796d3c0ab209992a99ce3f3faf6b303a852
Ashmeet <ashmeet.s.chhabra@gmail.com>
2024-02-17 10:03:59 -0600

    transferring to PC

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,8 @@ import StoryTimeline from "./StoryTimeline";
 function About() {
     return (
         <div id="about" className="text-5xl h-screen w-screen bg-orange-200 flex justify-center text-center">
-            
             <div>
-                <h1 className="text-7xl py-32">Our Story</h1>
+                <h1 className="text-7xl py-16">Our Story</h1>
                 <StoryTimeline/>
                 <h1>Something Here</h1>
             </div>
