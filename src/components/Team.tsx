d9761e265a850a72106a7ef0a365ecf464456633
DarkerCyan <k1914803@students.katyisd.org>
2024-02-17 18:30:26 -0600

    LMFAO LMFAO LMFAO LMFAO

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,35 +1,35 @@
 function Team() {
 
     return (
-        <div id="team" className="h-screen w-screen bg-orange-100 flex-col space-y-4 py-32">
+        <div id="team" className="h-full w-screen bg-orange-100 flex-col space-y-4 py-32">
             <h1 className="text-7xl text-center">Our Team</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 px-16 flex">
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
 
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
 
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
 
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
 
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
 
-                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center">
+                <div className="py-8 px-8 bg-emerald-600 rounded-2xl flex flex-col items-center justify-center opacity-75">
                     <img className="w-64 h-64 object-cover" src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" alt="Description of the image" />
                     <p className="mt-4 text-center">Some text underneath the image Some text underneath the image Some text underneath the image Some text underneath the image</p>
                 </div>
